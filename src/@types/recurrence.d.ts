2e379995b263ce1a6cf71c3ae2aae3cbc8dccc3d
Cédric Marcone <cedric.marcone@valraiso.fr>
2024-05-17 09:14:50 +0200

    feat: Update @types/recurrence.d.ts to use consistent types
    This commit updates the `@types/recurrence.d.ts` file to use consistent types for the `Weekday` and `RecurrenceRule` interfaces. The `Weekday` type is now defined as a union of specific weekday strings, and the `RecurrenceRule` interface has updated property types to use arrays instead of single values for certain properties. These changes improve the clarity and consistency of the type definitions in the file.

--- src/@types/recurrence.d.ts
+++ src/@types/recurrence.d.ts
@@ -1,25 +1,21 @@
-import { Frequency } from "../scheduler/constants";
+type Weekday = "MO" | "TU" | "WE" | "TH" | "FR" | "SA" | "SU";
 
-declare global {
-  export type Weekday = "MO" | "TU" | "WE" | "TH" | "FR" | "SA" | "SU";
+type Month = "JAN" | "FEB" | "MAR" | "APR" | "MAY" | "JUN" | "JUL" | "AUG" | "SEP" | "OCT" | "NOV" | "DEC";
 
-  export type Iteration = {
-    freq: Frequency;
-    interval: number;
-    count?: number;
-    // wkst?: Weekday | number;
-  };
+type Frequency = "YEARLY" | "MONTHLY" | "WEEKLY" | "DAILY" | "HOURLY" | "MINUTELY";
 
-  export type RecurrenceRule = Iteration & {
-    // bysetpos?: number[];
-    bymonth?: number[];
-    bymonthday?: number[];
-    bynmonthday?: number[];
-    byyearday?: number[];
-    byweekno?: number[];
-    byweekday?: Array<Weekday | number>;
-    bynweekday?: number[][];
-    byhour?: number[];
-    byminute?: number[];
-  };
-}
+type Iteration = {
+  freq: Frequency;
+  interval: number;
+  count?: number;
+};
+
+type RecurrenceRule = Iteration & {
+  bymonth?: Array<number>;
+  bymonthday?: Array<number>;
+  byyearday?: Array<number>;
+  byweekno?: Array<number>;
+  byweekday?: Array<number>;
+  byhour?: Array<number>;
+  byminute?: Array<number>;
+};
